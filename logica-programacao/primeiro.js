fdde5ddd6c435f71d53935c05a3248a3911ddd2f
CHDanielll <dancarvalho0808@gmail.com>
2025-11-10 15:09:18 -0300

    subindo js

---
+++ logica-programacao/primeiro.js
@@ -0,0 +1,14 @@
+// Perguntando o nome para o user
+const userName = prompt("Qual é seu nome??");
+
+
+// imprimindo na tela o resultado 
+document.write("Seu nome é: " + "<strong>" + userName + "</strong>" + "<br>");
+
+// Perguntando para o user a sua idade
+
+const userAge = prompt("Qual a sua idade?");
+
+// imprimindo na tela a idade
+
+document.write("Sua idade é: " + "<strong>" + userAge + "</strong>" + " Anos");
---
+++ logica-programacao/segundo.js
@@ -0,0 +1,4 @@
+let numero1 = prompt("Ditgite um numero");
+let numero2 = prompt("Digite outro numero");
+
+console.log = (numero1 + numero2)
