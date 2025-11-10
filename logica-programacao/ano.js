9287e03eb48484410733bbb7ab492e0537df011a
CHDanielll <dancarvalho0808@gmail.com>
2025-11-10 16:26:02 -0300

    subindo logica de programação (calculadora de anos da copa)

---
+++ logica-programacao/ano.js
@@ -0,0 +1,15 @@
+// criar um programa, para verificar o ano de nascimento 
+
+let idade = Number(prompt("Digite sua idade"));
+const anoAtual = 2025;
+/* 
+pegar o ano atual e subtrarir pela idade
+*/
+
+let anoNascimento = anoAtual - idade;
+
+document.write("Seu ano de nascimento é: <strong>" + anoNascimento + "</strong>");
+
+// Diego, 35 anos
+// juliana, 10 anos
+// joão, 17 anos
---
+++ logica-programacao/copa.js
@@ -0,0 +1,9 @@
+// Descobrir o ano da proxima Copa do Mundo
+let dataCopa = Number(prompt("Digite o ano da ultima"))
+const anoAtual = 2025;
+
+/*
+    a copa do mundo acontece a cada 4 anos
+    a última foi em 2022
+*/
+document.write("A proxima copa do mundo será em: " + (anoAtual + 1));
