6d248db7c393d0cc4b202c2a2ec447455dbdfe16
CHDanielll <dancarvalho0808@gmail.com>
2025-11-18 15:03:10 -0300

    subindo ai

--- smart-contatos/js/script.js
+++ smart-contatos/js/script.js
@@ -1,17 +1,28 @@
+
+const form = document.querySelector("form");
 const inputNome = document.getElementById("nome");
 const inputEmail = document.getElementById("email");
 const inputTelefone = document.getElementById("telefone");
 
-
-/*const inputTelefone = document.getElementById("nome", "email", "telefone");*/
-
-
-function cadastrado(event){
-    /*alert("Vc clicou em um botão");*/
-    console.log(event);
-    console.log(inputNome.value);
-    console.log(inputEmail.value);
-    console.log(inputTelefone.value);
+form.addEventListener("submit", function(event){
     event.preventDefault();
+
+
+//validação do formulario
+/*
+se o input for vazio, adicionar uma mensagem
+*/
+if (inputNome.value == "") {
+   alert("Digite um Nome:");
+   return false;    
 }
+
+
+
+    console.log("Olá mundo");
+    console.log("Nome", inputNome.value);
+    console.log("Email",inputEmail.value);
+    console.log("telefone",inputTelefone.value);
+})
+
 
