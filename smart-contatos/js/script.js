7d29e8cc5f437cd181f058ae129aa7e55dc38929
CHDanielll <dancarvalho0808@gmail.com>
2025-11-18 16:17:00 -0300

    subindo os bagui

--- smart-contatos/js/script.js
+++ smart-contatos/js/script.js
@@ -1,5 +1,6 @@
 
 const form = document.querySelector("form");
+const lista = document.querySelector(".lista")
 const inputNome = document.getElementById("nome");
 const inputEmail = document.getElementById("email");
 const inputTelefone = document.getElementById("telefone");
@@ -12,17 +13,54 @@ form.addEventListener("submit", function(event){
 /*
 se o input for vazio, adicionar uma mensagem
 */
-if (inputNome.value == "") {
-   alert("Digite um Nome:");
+if (inputNome.value == ""||inputNome.value == "" || inputNome.value == "") {
+   alert("Digite Seus dados");
    return false;    
 }
 
+/*if (inputNome.value == "") {
+   alert("Digite um Nome:");
+   return false;    
+}
+if (inputNome.value == "") {
+   alert("Digite um Nome:");
+   return false;    
+}*/
 
 
-    console.log("Olá mundo");
-    console.log("Nome", inputNome.value);
-    console.log("Email",inputEmail.value);
-    console.log("telefone",inputTelefone.value);
+/*while(){
+    inputNome=("")
+    inputEmail=("")
+    inputTelefone=("")
+    alert("Digite algo ai")
+    return false false;
+}*/
+
+
+
+    
+    /*console.log("Nome:", inputNome.value);
+    console.log("Email:",inputEmail.value);
+    console.log("telefone:",inputTelefone.value);*/
+
+    
+    // criar li
+    const li = document.createElement("li");
+
+    li.innerHTML = `
+    <span class="contato-nome">${inputNome.value}</span>
+    <span class="contato-nome">${inputEmail.value}</span>
+    <span class="contato-nome">${inputTelefone.value}</span>
+    `;
+    
+    console.log(li)
+
+    //append
+    lista.append(li)
+
+    //limpar oque ja foi escrito
+    form.reset();
 })
+
 
 
