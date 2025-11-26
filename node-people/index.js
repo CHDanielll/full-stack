72bc2ab3a986bc91d7c209b2099240063894d59f
CHDanielll <dancarvalho0808@gmail.com>
2025-11-26 16:15:27 -0300

    feat: Subindo uma nova rota principal

--- node-people/index.js
+++ node-people/index.js
@@ -11,16 +11,36 @@ const nomes = [
   { id: 5, nome: "Doris", idade: "70" },
 ];
 
+// Criando fuçãoes auxiliares
+// Retomar o objeto por id
+function buscarNomePorId(id){
+    return nomes.filter((nome) => nome.id == id)
+}
 
-app.listen(PORT, () => {
-  console.log("Servidor rodando no endereço http://localhost:${PORT}");
+
+
+app.get("/teste", (req, res) => {
+    res.send("API nodePeople está funcionando!");
+});
+app.get("/", (req, res) => {
+    res.send("digite um endereço valido");
 });
 
+
+
 // buscando nomes (listaNomes)
-app.get('/nomes',(req,res)=>{
+app.get('/ListaNomes',(req,res)=>{
     res.send(nomes);
 })
 
-app.get("/teste", (req, res) => {
-  res.send("API nodePeople está funcionando!");
+
+//buscando por id
+app.get("/listaNomes/:id", (req,res)=>{
+   let index = req.params.id;
+   
+   res.json(buscarNomePorId(index))
+})
+
+app.listen(PORT, () => {
+  console.log("Servidor rodando no endereço http://localhost:${PORT}");
 });
