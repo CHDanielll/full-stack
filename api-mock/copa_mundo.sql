af1db8a0760f9d3e519e59c8c70d81c158cddcc3
CHDanielll <dancarvalho0808@gmail.com>
2025-12-08 14:58:54 -0300

    feat: subindo rota para verificacao de itens do BD

---
+++ api-mock/copa_mundo.sql
@@ -0,0 +1,44 @@
+create database dadosUsuario;
+
+use dadosUsuario;
+
+create table dadosDeCadastro(
+id int auto_increment primary key,
+usuario varchar(30)not null,
+senha varchar(50)not null, 
+animalEstimacao varchar(100)not null
+);
+
+insert into dadosDeCadastro(usuario, senha, animalEstimacao) values
+('Eduardo Ramos', '@EuAmoMeuGato', 'Tenho um gato cinza'),
+('Juliana Clara', '@TenhoODogMaisFofo', 'Tenho um cachorro preto');
+
+select * from dadosDeCadastro;
+
+create table usuarioo(
+id int auto_increment primary key,
+email varchar(30)not null,
+senha varchar(100)not null, 
+sexo varchar(10)
+);
+
+insert into usuarioo(email, senha, sexo) values
+('Eduardo.ramos@gmail.com', '@EuAmoMeuGato', 'Homem'),
+('Juliana.claros@gmail.com', '@TenhoODogMaisFofo', 'Mulher');
+
+select * from usuarioo;
+
+create table dadosPessoais(
+id int auto_increment primary key,
+animalDeEstimacao varchar(30)not null,
+tomDePele varchar(10)not null, 
+idade varchar(30)not null
+);
+
+insert into dadosPessoais(animalDeEstimacao, tomDePele, idade) values
+('Cachorro', 'branca', '18 Anos'),
+('Gato', 'Parda', '22 Anos');
+
+select * from dadosPessoais;
+
+/*Só para salvar*/
--- api-mock/script.sql
+++ api-mock/script.sql
@@ -1,33 +1,28 @@
--- cria o banco de dados onde vamos trabalhar --
-create database sistema_produtos;
+-- cria o banco de dados
+create database copa_mundo;
 
-use sistema_produtos;
+use copa_mundo;
 
--- Cria a tabela 'produtos' com algumas colunas basicas
-
-create table produtos (
-id int auto_increment primary key, -- ID unico
-    nome varchar(100) not null, -- nome do produto
-    email varchar(120) not null, -- email (exemplo generico)
-    telefone varchar(20), -- telefone
-    cpf varchar(14), -- cpf
-    endereco varchar(150) -- endereco simples 
+-- Cria a tabela 'selecoes' com 3 colunas
+create table selecoes (
+ id int auto_increment primary key, -- ID unico que aumenta automaticamente
+ selecao varchar(100) not null, -- nome da selecao, texto até 100 caracteres
+ grupo char(1) not null
 );
--- inser dois registro
-insert into produtos(nome, email, telefone, cpf, endereco) values
-('Eduardo Ramos', 'edu.ramos@orutos.com', '11931831520', '323.234.428-93','Rua orutos F'),
-('juliana Clara', 'juliana@oruts.com', '11931831521', '122.313.131-32', 'Rua orutos');
 
--- seleciona o registro 
-select * from produtos where id = 1;
-select * from produtos where id = 2;
+select * from selecoes;
 
--- Deleta o registro 
-delete from produtos where id = 4;
+-- insere varias selecoes de uma vez na tabela
+insert into selecoes (selecao, grupo) values
+('Brasil', 'A'),
+('frança', 'B'),
+('ilhas de salomão', 'C'),
+('nigéria','D'),
+('Nova Zelandia', 'E'),
+('Japão', 'C');
 
--- Atualizar alguma informação 
-update produtos
-set nome = 'zeze campos dev'
-where id = 5;
+select * from selecoes;
 
-select * from produtos;
+
+
+
